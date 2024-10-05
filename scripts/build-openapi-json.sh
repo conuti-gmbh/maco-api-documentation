8fe4da74fe15c881876229970595011efe989a8b
DAS <daniel.schulz@conuti.de>
2024-10-05 22:13:10 +0200

    Update Events und lesende Schnittstellen

--- scripts/build-openapi-json.sh
+++ scripts/build-openapi-json.sh
@@ -1,11 +1,3 @@
-# build event-openapi.json
-swagger-cli bundle events-openapi.yaml --outfile _build/events-openapi.json --dereference --type json &&
-
-# minify event-openapi.json
-jq -c . _build/events-openapi.json > _build/events-openapi.min.json &&
-
-# remove event-openapi.json
-rm _build/events-openapi.json &&
 
 # build bo4e-openapi.json
 swagger-cli bundle bo4e-openapi.yaml --outfile _build/bo4e-openapi.json --dereference --type json &&
@@ -15,3 +7,12 @@ jq -c . _build/bo4e-openapi.json > _build/bo4e-openapi.min.json &&
 
 # remove bo4e-openapi.json
 rm _build/bo4e-openapi.json
+
+# build event-openapi.json
+swagger-cli bundle events-openapi.yaml --outfile _build/events-openapi.json --dereference --type json &&
+
+# minify event-openapi.json
+jq -c . _build/events-openapi.json > _build/events-openapi.min.json &&
+
+# remove event-openapi.json
+rm _build/events-openapi.json &&
